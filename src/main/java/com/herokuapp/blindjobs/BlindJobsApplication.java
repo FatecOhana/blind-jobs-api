18eb025dad79f23526e64583fad07e80709d7ca7
Guilherme Peres Lins da Palma <guippalma@gmail.com>
2022-08-29 20:31:28 -0300

    initial commit

---
+++ src/main/java/com/herokuapp/blindjobs/BlindJobsApplication.java
@@ -0,0 +1,13 @@
+package com.herokuapp.blindjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlindJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlindJobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herokuapp/blindjobs/BlindJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herokuapp.blindjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlindJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
